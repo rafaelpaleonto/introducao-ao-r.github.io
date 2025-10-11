39897eda8716a65a3c7ca9cd4c6119123cb072d5
Rafael Simões <rafael.oliveira3@sou.ufmt.br>
2025-10-11 10:02:55 -0400

    Create exemplos_variaveis.r

---
+++ introducao_ao_r/exemplos_variaveis.r
@@ -0,0 +1,19 @@
+### Exemplos de variáveis do tipo numeric
+V1 <- 3.14
+V2 <- 7.7452
+V3 <- -89.5236
+### Exemplos de variáveis do tipo double
+D1 <- 2.718281828459045
+D2 <- 3.14159265
+D3 <- -89.12345689622
+### Exemplos de variáveis do tipo integer
+I1 <- 10L
+I2 <- -85L
+I3 <- 1223L
+### Exemplos de variáveis do tipo booleano
+V <- TRUE
+F <- FALSE
+### Exemplos de variáveis do tipo booleano
+C1 <- "OLÁ ESTOU USANDO UMA IDE"
+C2 <- "EU SOU O R"
+C3 <- "CONHEÇA O PROJETO CONEXÃO NUPES"
